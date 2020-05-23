4e9008d4999d86a8229011eba9896781e20928b0
Donald Shen <825870831@qq.com>
2020-05-23 22:47:50 +0800

    步入 createFnInvoker
    持续一头雾水

--- src/core/vdom/helpers/update-listeners.js
+++ src/core/vdom/helpers/update-listeners.js
@@ -1,15 +1,10 @@
 // /* @flow */
 
-// import {
-//   warn,
-//   invokeWithErrorHandling
-// } from 'core/util/index'
 import {
-  cached,
-//   isUndef,
-//   isTrue,
-//   isPlainObject
-} from 'shared/util'
+  warn,
+  //   invokeWithErrorHandling
+} from 'core/util/index'
+import {cached, isUndef, isTrue} from 'shared/util'
 
 const normalizeEvent = cached((name) => {
   const passive = name.charAt(0) === '&'
@@ -26,22 +21,22 @@ const normalizeEvent = cached((name) => {
   }
 })
 
-// export function createFnInvoker (fns: Function | Array<Function>, vm: ?Component): Function {
-//   function invoker () {
-//     const fns = invoker.fns
-//     if (Array.isArray(fns)) {
-//       const cloned = fns.slice()
-//       for (let i = 0; i < cloned.length; i++) {
-//         invokeWithErrorHandling(cloned[i], null, arguments, vm, `v-on handler`)
-//       }
-//     } else {
-//       // return handler return value for single handlers
-//       return invokeWithErrorHandling(fns, null, arguments, vm, `v-on handler`)
-//     }
-//   }
-//   invoker.fns = fns
-//   return invoker
-// }
+export function createFnInvoker(fns, vm) {
+  function invoker() {
+    const fns = invoker.fns
+    if (Array.isArray(fns)) {
+      const cloned = fns.slice()
+      for (let i = 0; i < cloned.length; i++) {
+        invokeWithErrorHandling(cloned[i], null, arguments, vm, `v-on handler`)
+      }
+    } else {
+      // return handler return value for single handlers
+      return invokeWithErrorHandling(fns, null, arguments, vm, `v-on handler`)
+    }
+  }
+  invoker.fns = fns
+  return invoker
+}
 
 export function updateListeners(on, oldOn, add, remove, createOnceHandler, vm) {
   let name, def, cur, old, event
@@ -49,29 +44,29 @@ export function updateListeners(on, oldOn, add, remove, createOnceHandler, vm) {
     def = cur = on[name]
     old = oldOn[name]
     event = normalizeEvent(name)
-    //   if (isUndef(cur)) {
-    //     process.env.NODE_ENV !== 'production' &&
-    //       warn(
-    //         `Invalid handler for event "${event.name}": got ` + String(cur),
-    //         vm,
-    //       )
-    //   } else if (isUndef(old)) {
-    //     if (isUndef(cur.fns)) {
-    //       cur = on[name] = createFnInvoker(cur, vm)
-    //     }
-    //     if (isTrue(event.once)) {
-    //       cur = on[name] = createOnceHandler(event.name, cur, event.capture)
-    //     }
-    //     add(event.name, cur, event.capture, event.passive, event.params)
-    //   } else if (cur !== old) {
-    //     old.fns = cur
-    //     on[name] = old
-    //   }
+    if (isUndef(cur)) {
+      process.env.NODE_ENV !== 'production' &&
+        warn(
+          `Invalid handler for event "${event.name}": got ` + String(cur),
+          vm,
+        )
+    } else if (isUndef(old)) {
+      if (isUndef(cur.fns)) {
+        cur = on[name] = createFnInvoker(cur, vm)
+      }
+      if (isTrue(event.once)) {
+        cur = on[name] = createOnceHandler(event.name, cur, event.capture)
+      }
+      add(event.name, cur, event.capture, event.passive, event.params)
+    } else if (cur !== old) {
+      old.fns = cur
+      on[name] = old
+    }
   }
-  // for (name in oldOn) {
-  //   if (isUndef(on[name])) {
-  //     event = normalizeEvent(name)
-  //     remove(event.name, oldOn[name], event.capture)
-  //   }
-  // }
+  for (name in oldOn) {
+    if (isUndef(on[name])) {
+      event = normalizeEvent(name)
+      remove(event.name, oldOn[name], event.capture)
+    }
+  }
 }
